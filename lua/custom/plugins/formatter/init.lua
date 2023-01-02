cef32026a86a2f9de720e5add12c504b756b6edc
Tin Nguyen <hopthucuatin@gmail.com>
2023-01-02 21:45:34 +0700

    update

---
+++ lua/custom/plugins/formatter/init.lua
@@ -0,0 +1,57 @@
+-- Utilities for creating configurations
+local util = require "formatter.util"
+
+-- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+require("formatter").setup {
+  -- Enable or disable logging
+  logging = true,
+  -- Set the log level
+  log_level = vim.log.levels.WARN,
+  -- All formatter configurations are opt-in
+  filetype = {
+    -- Formatter configurations for filetype "lua" go here
+    -- and will be executed in order
+    lua = {
+      require("formatter.filetypes.lua").stylua,
+    },
+
+    rust = {
+      require("formatter.filetypes.rust").rustfmt,
+    },
+
+    typescript = {
+      require("formatter.filetypes.typescript").prettier,
+    },
+
+    typescriptreact = {
+      require("formatter.filetypes.typescript").prettier,
+    },
+
+    javascript = {
+      require("formatter.filetypes.javascript").prettier,
+    },
+
+    javascriptreact = {
+      require("formatter.filetypes.javascript").prettier,
+    },
+
+    css = {
+      require("formatter.filetypes.css").prettier,
+    },
+
+    -- Use the special "*" filetype for defining formatter configurations on
+    -- any filetype
+    ["*"] = {
+      -- "formatter.filetypes.any" defines default configurations for any
+      -- filetype
+      require("formatter.filetypes.any").remove_trailing_whitespace,
+    },
+  },
+}
+
+vim.cmd [[
+augroup FormatAutogroup
+  autocmd!
+  autocmd BufWritePost * FormatWrite
+augroup END
+]]
